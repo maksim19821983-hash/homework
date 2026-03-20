d828af3ff114c830a1cad1553426f6fd34743a4a
maksim19821983-hash <maksim19821983@gmail.com>
2026-03-20 03:02:18 +0300

    Add files via upload

---
+++ lesson21/main.js
@@ -0,0 +1,20 @@
+//Задача 1
+
+const FIRST_NAME = 'Maksim';
+const LAST_NAME = 'Remezov';
+const ISSTUDENT = true;
+
+//Задача 2
+
+let age = 18;
+let currentYear = 2026;
+let birthYear =  currentYear - age;
+
+
+//Задача 3 
+
+console.log("Меня зовут",FIRST_NAME, LAST_NAME, "мне", age, "лет. Я ученик курса: ",ISSTUDENT)
+
+
+
+console.log ("выведется строка '123456789falsetrue'") 
